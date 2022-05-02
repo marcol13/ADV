32f0e5fb611cf52f639bd4e895d03e3700a74479
Marcin Krueger <marcinkrueger@gmail.com>
2022-05-02 23:24:25 +0200

    node page patterns

---
+++ Node/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+const path = require("path");
+
+const app = express();
+const port = 8003;
+
+app.use(express.static("public"));
+
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+);
+
+app.use(expressLayouts);
+app.set("layout", "./layout");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("main");
+});
+
+app.get("/shopping-cart", (req, res) => {
+  res.render("shopping-cart", {
+    myCart: [{ item: "Gitara", amount: 3, price: 500 }],
+  });
+});
+
+app.listen(port);
